2eab6338182bac7c6018e0364f82ef6019c20c62
meron1921 <merondejene1921@gmail.com>
2022-08-31 13:23:25 -0700

    0x02-python-import_modules

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,13 +1,19 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     import sys
-    if (len(sys.argv) == 1):
-        print(f"{len(sys.argv)-1} arguments.")
-    elif (len(sys.argv) == 2):
-        print(f"{len(sys.argv)-1} argument:")
-        print(f"{len(sys.argv)-1}: {sys.argv[1]}")
+    i = len(sys.argv) - 1
+
+    if i == 0:
+        print("{} arguments.".format(i))
+    elif i == 1:
+        print("{} argument:".format(i))
     else:
-        print(f"{len(sys.argv)-1} arguments:")
-        for i in range(len(sys.argv)-1):
-            print(f"{i+1}: {sys.argv[i+1]}")
+        print("{} arguments:".format(i))
+
+    if i >= 1:
+        i = 0
+        for arg in sys.argv:
+            if i != 0:
+                print("{}: {}".format(i, arg))
+            i += 1
 
--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
+    import sys
     import hidden_4
-    names = dir(hidden_4)
-    for i in names:
-        if (i[:2] != "__"):
-            print(i)
+    for n in dir(hidden_4):
+        if n[:2] != "__":
+            print(n)
