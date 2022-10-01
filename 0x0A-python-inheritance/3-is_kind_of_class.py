24891dbf95dfb5d2fddc7c54c80fa8b8d79e610f
meron1921 <merondejene1921@gmail.com>
2022-10-01 05:03:58 -0700

    0x0A-python-inheritance

---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+""" check inheirt from specified class """
+
+
+def is_kind_of_class(obj, a_class):
+    """ return True if inherit from specified class otherwise False"""
+    return isinstance(obj, a_class)
+Footer
+© 2022 GitHub, Inc.
+Footer navigation
+Terms
+Privacy
+S
